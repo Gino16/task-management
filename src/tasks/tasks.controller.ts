639f48b857223099af232aa8c2a769a1ab741be9
Gino Ascencio <gino.francisco@hotmail.com>
2022-01-07 18:33:26 -0500

    ADD Guards to task controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,13 +7,15 @@ import {
   Post,
   Put,
   Query,
+  UseGuards,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-task-filter.dto';
 import { UpdateTaskStatus } from './dto/update-task-status.dto';
 import { Task } from './task.entity';
 import { TasksService } from './tasks.service';
-
+@UseGuards(AuthGuard())
 @Controller('tasks')
 export class TasksController {
   constructor(private taskService: TasksService) {}
